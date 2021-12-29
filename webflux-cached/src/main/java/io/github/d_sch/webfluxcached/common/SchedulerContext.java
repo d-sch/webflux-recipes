95a8c494b26a8d1ec4fcf61768aaa61bed69e7a8
d-sch <ab_cd_de@outlook.com>
2021-12-29 20:07:15 +0100

    Added license headers switch to apache 2.0

--- webflux-cached/src/main/java/io/github/d_sch/webfluxcached/common/SchedulerContext.java
+++ webflux-cached/src/main/java/io/github/d_sch/webfluxcached/common/SchedulerContext.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 - 2021 d-sch
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and,
+ * limitations under the License.
+ */
+
 package io.github.d_sch.webfluxcached.common;
 
 import java.util.concurrent.Executor;
@@ -11,6 +27,18 @@ import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 
 @Builder
+/**
+ * SchedulerContext
+ * 
+ * Ensure specific scheduler for publisher and subscriber thread execution on entry
+ * and specific scheduler for publisher and subscriber afterwards.
+ * 
+ * If you have to derend on global resources and you require synchronized access.
+ * 
+ * One example whould be to create a single thread scheduler to make sure only one
+ * thread is able to access a resource sequentially.
+ * 
+ */
 public final class SchedulerContext {
 
     @Getter
@@ -43,6 +71,7 @@ public final class SchedulerContext {
         return flux
             //Ensure single cache thread
             .publishOn(schedulerBefore)
+            .subscribeOn(schedulerBefore)
             .transform(transformer)
             //Publish result outside of cache thread
             .publishOn(schedulerAfter)
