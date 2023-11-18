21f30a1b1575dc75b960de85137cf5f557a07193
d-sch <ab_cd_de@outlook.com>
2023-11-18 14:45:02 +0100

    Cleanup uncommited changes

--- webflux-recipes-examples/src/main/java/io/github/d_sch/webfluxrecipes/examples/config/R2DBCConfiguration.java
+++ webflux-recipes-examples/src/main/java/io/github/d_sch/webfluxrecipes/examples/config/R2DBCConfiguration.java
@@ -5,14 +5,15 @@ import org.springframework.boot.r2dbc.ConnectionFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.d_sch.webfluxconfig.config.AbstractR2DBCConfiguration;
+import io.github.d_sch.webfluxconfig.config.R2DBCConfigurationProperties;
 import io.r2dbc.spi.ConnectionFactory;
 
 @Configuration
-public class R2DBCConfiguration extends AbstractR2DBCConfiguration {
+@EnableConfigurationProperties(R2DBCConfigurationProperties.class)
+public class R2DBCConfiguration extends R2DBCConfigurationProperties {
     
     @Bean
-    ConnectionFactory getDemoConnectionFactory(AbstractR2DBCConfiguration properties) {
+    ConnectionFactory getDemoConnectionFactory(R2DBCConfigurationProperties properties) {
         return ConnectionFactoryBuilder
             .withOptions(buildConnectionFactoryOptions(properties.getOptions().get("demo"))
         ).build();
