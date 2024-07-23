ec97767aa42693f8cf9bf597bf15b7ce8657dad5
d-sch <d-sch@users.noreply.github.com>
2024-07-23 23:32:54 +0200

    Adding .devcontainer, Add cancel propagation (#4)

--- webflux-custom-jackson-stream/src/test/java/io/github/d_sch/webfluxcustomjacksonstream/FluxJsonGeneratorTest.java
+++ webflux-custom-jackson-stream/src/test/java/io/github/d_sch/webfluxcustomjacksonstream/FluxJsonGeneratorTest.java
@@ -3,6 +3,9 @@ package io.github.d_sch.webfluxcustomjacksonstream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.buffer.DataBuffer;
@@ -10,6 +13,7 @@ import org.springframework.core.io.buffer.DataBuffer;
 import io.github.d_sch.webfluxcustomjacksonstream.common.JsonWriter;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 @Slf4j
@@ -18,11 +22,23 @@ public class FluxJsonGeneratorTest {
     @Test
     public void test() {
         Flux<DataBuffer> source = Flux.defer(() -> Flux.range(1, 5))
-            .as(inFlux -> JsonWriter.asDataBufferFlux(inFlux));
+            .transform(inFlux -> JsonWriter.asDataBufferFlux(inFlux));
         
         StepVerifier.create(source).assertNext(
             actual -> assertEquals("[1,2,3,4,5]", actual.toString(StandardCharsets.UTF_8))
         ).verifyComplete();
+    }
+
+    @Test
+    public void test2() {
+        Flux<DataBuffer> source = Flux.defer(() -> Flux.never())
+            .transform(inFlux -> JsonWriter.asDataBufferFlux(inFlux))
+            .timeout(Duration.of(1, ChronoUnit.SECONDS));        
+
+        StepVerifier
+            .create(source)
+            .expectError(TimeoutException.class)
+            .verify();
     }
 
 }
